bdbc8c3a870b400c8385b3dbed64c345960f10bc
JUNGSANGYUN <sdsd98987@gmail.com>
2024-10-12 20:12:16 +0900

    frontend 공부

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
+const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
+const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
-
-const newsFeed = JSON.parse(ajax.response);
+const newsFeed = getData(NEWS_URL);
 const ul = document.createElement('ul');
 
+window.addEventListener('hashchange', function() {
+  const id = location.hash.substring(1)
+  const newsContent = getData( CONTENT_URL.replace('@id',id))
+  const title = document.createElement('h1');
+
+  title.innerText = newsContent.title;
+  content.appendChild(title);
+});
+
 for(let i = 0; i < 10; i++) {
+  const div = document.createElement('div');
   const li = document.createElement('li');
+  const a = document.createElement('a');
 
-  li.innerHTML = newsFeed[i].title;
+  div.innerHTML =  `
+  <li>
+    <a href="#${newsFeed[i].id}">
+    ${newsFeed[i].title} (${newsFeed[i].comments_count})
+    </a>
+  </li>
+  `;
 
-  ul.appendChild(li);
+  ul.appendChild(div.firstElementChild);
+
 }
 
-document.getElementById('root').appendChild(ul);
+
+function getData(url) {
+  ajax.open('GET', url, false);
+  ajax.send();
+
+  return JSON.parse(ajax.response);
+}
+
+container.appendChild(ul);
+container.appendChild(content);
