d3c2270ebc49ff3b3633c134951b0ecd1b1f968e
JUNGSANGYUN <sdsd98987@gmail.com>
2024-10-23 17:21:42 +0900

    frontend 공부 typescript 적용

---
+++ tsconfig.js
@@ -0,0 +1,12 @@
+{
+    "compilerOptions": {
+        "strict" : true,
+        "target" :"ES5",
+        "module" : "CommonJS",
+        "alwaysStrict" : true,
+        "noImplicitAny" : true,
+        "noImplictThis" : true,
+        "sourceMap" : true,
+        "downlevelIteration": true
+    }
+}
