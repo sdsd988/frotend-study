ee8ff21aafe5558457e65982c08a81b5725ad297
JUNGSANGYUN <sdsd98987@gmail.com>
2024-11-14 16:21:12 +0900

    프로젝트 구조 변경

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import Router from "./core/router";
+import {Store} from "./types";
+import {NewsDetailView, NewsFeedView} from "./page";
+
+const store: Store = {
+    currentPage: 1,
+    feeds: [],
+};
+
+
+const router: Router = new Router();
+const newsFeedView = new NewsFeedView('root');
+const newsDetailView = new NewsDetailView('root');
+
+router.setDefaultPage(newsFeedView);
+
+router.addRoutePath('/page/', newsFeedView);
+router.addRoutePath('/show/', newsDetailView);
+
+router.route();
